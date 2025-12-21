ca7d70aab14ff2529ad2a0ba49d154897a380170
phrichner <ph.richner@gmail.com>
2025-12-21 12:44:20 +0100

    Fix build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto-tracker/', // Replace with your repository name
+  base: '/crypto-tracker/',
   build: {
     outDir: 'dist',
     sourcemap: false,
-  },
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY || ''),
   },
 });
